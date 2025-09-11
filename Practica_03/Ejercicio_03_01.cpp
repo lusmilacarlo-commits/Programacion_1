22b17b4da8bfb2fb0f9e4f4662bc05e86800920e
lusmilacarlo-commits <lusmila.carlo@ucb.edu.bo>
2025-09-11 08:39:57 -0400

    Add files via upload
    Ejercicio 1 de la practica 3

---
+++ Practica_03/Ejercicio_03_01.cpp
@@ -0,0 +1,32 @@
+// Materia: Programacion I, Paralelo 4
+// Autor: Lusmila Abigail Carlo Quispe
+// Carnet:10907331 L.P.
+//Carrera del estudiante: Ingenieria Mecatrónica
+// Fecha de creación: 10/09/2025
+//Número de ejercicio: 7
+
+#include <iostream>
+using namespace std;
+
+// función que devuelve un entero: 1 si es par, 0 si es impar
+int esPar(int numero) 
+{
+    if (numero % 2 == 0)
+        return 1; // verdadero, es par
+    else
+        return 0; // falso, es impar
+}
+
+int main() 
+{
+    int n;
+    cout << "Ingrese un numero: ";
+    cin >> n;
+
+    if (esPar(n))
+        cout << "El numero es par" << endl;
+    else
+        cout << "El numero es impar" << endl;
+
+    return 0;
+}
